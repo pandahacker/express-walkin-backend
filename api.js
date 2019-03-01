a53434b524b50fc97627975e6cba19cb0e12f811
Blake Albertson <albertson.blake@yahoo.com>
2019-03-01 16:50:41 -0500

    add endpoint for editing records

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const WalkIn = require('./models/walkin')
 
 //get people that have signed into the front desk
 router.get('/signedin', function (req, res, next){
-    WalkIn.find({ claimed: false }).then(function(walkins){
+    WalkIn.find({office:req.query.loc, claimed: false}).then(function(walkins){
         res.send(walkins);
     }).catch(next);
 });
@@ -18,8 +18,10 @@ router.post('/register', function (req, res, next) {
 });
 
 //modify existing walk in
-router.put('/register/:id', function (req, res, next) {
-    res.send({ type: 'PUT' });
+router.put('/claim/:id', function (req, res, next) {
+    WalkIn.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(walkins){
+        res.send(walkins);
+    }).catch(next);
 });
 
 module.exports = router;
