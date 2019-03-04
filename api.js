1272ab09fd2651c0d5171c02ff21603eedec9064
Blake Albertson <albertson.blake@yahoo.com>
2019-03-04 15:14:43 -0500

    add logging to server

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const WalkIn = require('./models/walkin')
 //get people that have signed into the front desk
 router.get('/signedin', function (req, res, next){
     WalkIn.find({office:req.query.loc, claimed: false}).then(function(walkins){
+        console.log(`retreiving sign ins from ${req.query.loc}`);
         res.send(walkins);
     }).catch(next);
 });
@@ -13,6 +14,7 @@ router.get('/signedin', function (req, res, next){
 //add a new member into the queue
 router.post('/register', function (req, res, next) {
     WalkIn.create(req.body).then(function(walkins){
+        console.log(`Adding user ${req.body.name}`);
         res.send(walkins);
     }).catch(next);
 });
