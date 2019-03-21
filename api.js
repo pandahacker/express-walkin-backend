cd8e0d851d5449c2fcfb015e6d7610917d929729
Blake Albertson <albertson.blake@yahoo.com>
2019-03-21 13:11:51 -0400

    update api

--- api.js
+++ api.js
@@ -5,8 +5,23 @@ const WalkIn = require('./models/walkin')
 
 //get people that have signed into the front desk
 router.get('/signedin', function (req, res, next){
-    WalkIn.find({office:req.query.loc, claimed: false}).then(function(walkins){
-        console.log(`retreiving sign ins from ${req.query.loc}`);
+    if (req.query.loc == undefined){
+        WalkIn.find({claimed: true}).then(function (walkins) {
+            console.log(`retreiving sign ins`);
+            res.send(walkins);
+        }).catch(next);
+    }
+    else {
+        WalkIn.find({ office: req.query.loc, claimed: false }).then(function (walkins) {
+            console.log(`retreiving sign ins from ${req.query.loc}`);
+            res.send(walkins);
+        }).catch(next);
+    }
+    
+});
+router.get('/reporting', function (req, res, next) {
+    WalkIn.find({claimed: true }).then(function (walkins) {
+        console.log(`retreiving past walk ins`);
         res.send(walkins);
     }).catch(next);
 });
